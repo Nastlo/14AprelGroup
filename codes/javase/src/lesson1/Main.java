e5fb8bba22ec4a8fc364616f13a5a28cc04e3a92
Nastlo <omarcafarli60@gmail.com>
2025-10-27 17:12:53 +0400

    Java Project

---
+++ codes/javase/src/lesson1/Main.java
@@ -0,0 +1,11 @@
+package lesson1;
+
+public class Main {
+
+	public static void main(String[] args) {
+        System.out.println("Omar");
+        System.out.println("Jafarli");
+        System.out.println("13");
+	}
+
+}
