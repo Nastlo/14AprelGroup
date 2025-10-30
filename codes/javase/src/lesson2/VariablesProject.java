10fc27ed1375575e084a273bf6f6bef9ab452c3f
Nastlo <omarcafarli60@gmail.com>
2025-10-30 18:43:16 +0400

    lesson3

---
+++ codes/javase/src/lesson2/VariablesProject.java
@@ -0,0 +1,23 @@
+package lesson2;
+public class VariablesProject {
+
+    public static void main(String[] args) {
+            int age = 20;
+            System.out.println("age: " + age);
+
+            boolean hasHome = true;
+            System.out.println("hasHome: " + hasHome);
+
+
+            int studentGrade = 4;
+            System.out.println("studentGrade: " + studentGrade);
+
+            double productPrice = 3.8;
+            System.out.println("productPrice: " + productPrice);
+
+            String studentName = "Yaqub";
+            System.out.println("studentName: " + studentName);
+
+    }
+
+}
---
+++ codes/javase/src/lesson3/Main.java
@@ -0,0 +1,26 @@
+// VariablesPracticeHome.java
+
+public class Main {
+    public static void main(String[] args) {
+        
+n
+        int age = 25;
+        System.out.println("age : " + age);
+
+
+        boolean hasJob = false;
+        System.out.println("hasJob : " + hasJob);
+
+
+        char studentGrade = 'C';
+        System.out.println("studentGrade : " + studentGrade);
+
+
+        double carPrice = 25000;
+        System.out.println("carPrice : " + carPrice);
+
+
+        String employeeName = "Məlik";
+        System.out.println("employeeName : " + employeeName);
+    }
+}
