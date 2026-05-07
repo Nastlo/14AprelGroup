421b3bebb80978cf00ff3a682e97b68e72e98503
Nastlo <omarcafarli60@gmail.com>
2026-05-07 18:26:38 +0400

    task

--- codes/spring/spring-project-14aprel/spring-project-14aprel/src/main/java/az/developia/spring_project_14aprel/SpringProject14aprelApplication.java
+++ codes/spring/spring-project-14aprel/spring-project-14aprel/src/main/java/az/developia/spring_project_14aprel/SpringProject14aprelApplication.java
@@ -2,20 +2,25 @@ package az.developia.spring_project_14aprel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.ApplicationContext;
 
-import az.developia.spring_project_14aprel.entity.Person;
+import az.developia.spring_project_14aprel.entity.Home;
 
 @SpringBootApplication
 public class SpringProject14aprelApplication {
 
     public static void main(String[] args) {
 
-        ConfigurableApplicationContext context =
+        ApplicationContext context =
                 SpringApplication.run(SpringProject14aprelApplication.class, args);
 
-        Person person = context.getBean(Person.class);
+        Home home1 = context.getBean(Home.class);
+        Home home2 = context.getBean(Home.class);
 
-        context.close();
+        System.out.println(home1);
+        System.out.println(home2);
+
+        System.out.println(home1 == home2);
     }
+
 }
